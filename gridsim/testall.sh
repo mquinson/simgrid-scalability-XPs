46689012f75bb29ab384a09ae4143d1170dd38f0
Martin Quinson (rennes) <martin.quinson@loria.fr>
2011-11-16 14:23:33 +0100

    ensure that several instances can run from the same dir (+cleanups)

--- gridsim/testall.sh
+++ gridsim/testall.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 timefmt="clock:%e user:%U sys:%S swapped:%W exitval:%x max:%Mk avg:%Kk # %C"
+me=tmp/`hostname -s`
 
 maxtasks=1000000
 maxslaves=5000
@@ -15,45 +16,35 @@ function roll() {
   echo $res
 }
 
-function create_test_uniform() {
+
+for i in `seq 1 1000` ; do
+  echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+  echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
   slave=`roll $maxslaves`
   task=`roll $maxtasks`
   jobsize=`roll $maxjobsize`
   infilesize=`roll $maxfileINsize`
   outfilesize=`roll $maxfileOUTsize`
-}
-  
-for i in `seq 1 1000` ; do
- create_test_uniform
- echo slave:$slave task:$task jobsize:$jobsize infilesize:$infilesize outfilesize:$outfilesize
-#done
-#exit 0
+  echo slave:$slave task:$task jobsize:$jobsize infilesize:$infilesize outfilesize:$outfilesize
 
-#for task in 10 500 1000 5000 10000 50000 100000 500000 1000000 ; do 
-#  for slave in 10 500 1000 5000 ; do # 10000 50000 100000 500000 1000000 ; do 
-#    for jobsize in 10 1000000; do # 10000 1000000 ; do
-#      for infilesize in 10 1000000; do # 10000 1000000 ; do
-#        for outfilesize in 10 1000000; do # 10000 1000000 ; do
-    
-#
-  echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-  echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-  
 
-  killall -KILL java 
+  killall -KILL java 2>/dev/null
   # Wrong number of args. Usage : number of jobs, job size, number of hosts, input file size for job, output file size for job
-  /usr/bin/time -o timings  java -Xmx46g -cp gridSim.jar:lib/simjava2.jar:. RunningMasterSlaves  $task $jobsize $slave $infilesize $outfilesize 
-  
-  if grep "Command terminated by signal" timings ; then 
+  /usr/bin/time -f "$timefmt" -o $me.timings  java -Xmx46g -cp gridSim.jar:lib/simjava2.jar:. RunningMasterSlaves  $task $jobsize $slave $infilesize $outfilesize 
+
+  if grep "Command terminated by signal" $me.timings ; then
+    echo "Damn, error detected:"
+  elif grep "Command exited with non-zero status" $me.timings ; then
     echo "Damn, error detected:"
   else
     echo
     echo "#Legend: slave task jobsize infilesize outfilesize wallclock(in sec) usertime systemtime swapoutamount exitstatus # commandline used"
     echo -n "XXXX PRECIOUS_RESULT XXXX $slave $task $jobsize $infilesize $outfilesize "
   fi
-  cat timings
+  cat $me.timings
 
-  killall java
+  killall java 2>/dev/null
   done
   done
 done
