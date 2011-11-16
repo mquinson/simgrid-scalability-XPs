ac855ca50edfe443a4f57ddfc82a7927739c6bb3
Martin Quinson (rennes) <martin.quinson@loria.fr>
2011-11-16 12:11:08 +0100

    version bien plus complete, avec lancement et nettoyage

--- get_a_machine.sh
+++ get_a_machine.sh
@@ -1,4 +1,10 @@
-
-oarsub -I -l 'nodes=1,walltime=1' -p "cluster='graphene'" -t deploy 
+dtach -A my-dtach-socket bash
+oarsub -I -l 'nodes=1,walltime=10' -p "cluster='graphene'" -t deploy 
+oarsub -I -l 'nodes=1,walltime=10' -p "cluster='parapluie'" -t deploy 
 kadeploy3 -e squeeze-x64-nfs -f $OAR_NODE_FILE 
+for node in `cat $OAR_NODE_FILE|sort -u` ; do ssh root@$node "sh ~mquinson/tuneit" ; done
+
+for n in `sort -u $OAR_NODE_FILE` ; do ssh $n "rm /tmp/mq-dtach;killall testall.sh java time run.gridsim;sleep 1; kill -KILL -1" ; done; killall ssh
+for n in `sort -u $OAR_NODE_FILE` ; do echo "dtach -c /tmp/mq-dtach ./run.gridsim" | ssh -tt -o StrictHostKeyChecking=no -o BatchMode=yes  $n &  done
+
 ssh `head -1 $OAR_NODE_FILE`
