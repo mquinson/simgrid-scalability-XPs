ada659ac8b96bb31bdea87d8a96302e6e9153c15
Martin Quinson (toulouse) <martin.quinson@loria.fr>
2011-11-25 10:53:00 +0100

    make sure that we don't hit the swap during the experiments

--- tune_machine.sh
+++ tune_machine.sh
@@ -2,6 +2,8 @@ apt-get -y install debconf-utils
 echo "sun-java6-bin   shared/accepted-sun-dlj-v1-1    boolean true" | debconf-set-selections
 apt-get -y install cmake-curses-gui libpcre3-dev sun-java6-jdk dtach git-core jed dc cpufrequtils
 
+swapoff -va
+
 if hostname |grep -q pastel ; then
   echo "Changing the CPU freq to 1000MHz because we are on pastel"
   cpufreq-set -r -d 1000MHz -u 1000MHz -c 0 
